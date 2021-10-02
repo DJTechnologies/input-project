c5fa0032443b541b74a568cfc799e324feca2e6b
MJ <Juliana.technologies.83@gmail.com>
2021-10-02 18:53:38 -0400

    my function

---
+++ index.js
@@ -0,0 +1,19 @@
+// function name(parmameters) {
+//     value
+// }
+
+function addition(a, b) {
+  return a + b;
+}
+
+function subtraction(c, d) {
+  return c - d;
+}
+
+function multiplication(e, f) {
+  return e * f;
+}
+
+function divide(g, h) {
+  return g / h;
+}

b79c80ce1480bd977d48da555163b590c6e9afde
MJ <Juliana.technologies.83@gmail.com>
2021-10-02 20:43:10 -0400

    query

--- index.js
+++ index.js
@@ -2,18 +2,32 @@
 //     value
 // }
 
+// function addition(a, b) {
+//   return a + b;
+// }
+
+// function subtraction(c, d) {
+//   return c - d;
+// }
+
+// function multiplication(e, f) {
+//   return e * f;
+// }
+
+// function divide(g, h) {
+//   return g / h;
+// }
+
+// dom manipulation
+// const parentElement = document.querySelector("#parent");
+
+// grab input box from html
+const inputOne = document.querySelector("#input1");
+const inputTwo = document.querySelector("#input2");
+const labelOne = document.querySelector("#result");
+
+// function for my addition
 function addition(a, b) {
   return a + b;
 }
-
-function subtraction(c, d) {
-  return c - d;
-}
-
-function multiplication(e, f) {
-  return e * f;
-}
-
-function divide(g, h) {
-  return g / h;
-}
+addition(inputOne, inputTwo);
